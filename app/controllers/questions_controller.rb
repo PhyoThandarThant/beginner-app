5d8bf9cbc042de18e27290c840e95ec8dccc8ba4
PhyoTDT <phyothandarthant@cybermissions.co.jp>
2021-11-18 22:35:29 +0900

    question crud operation

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,6 @@
 class QuestionsController < ApplicationController
+    before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+    before_action :ensure_correct_user, only: [:edit, :update, :destroy]
     def index 
         @test = "テストテキスト"
     end
@@ -19,8 +21,30 @@ class QuestionsController < ApplicationController
         end
     end
 
+    def edit
+        @question = Question.find(params[:id])
+    end
+
+    def update
+        @question = Question.find(params[:id])
+        if @question.update(question_params)
+            flash[:notice] ="成功"
+            redirect_to ("/questions/#{@question.id}")
+        else
+            flash.now[:alert] = "失敗"
+            render("questions/edit")
+        end
+    end
+
     def show
         @question = Question.find(params[:id])
+    end
+
+    def destroy
+        @question = Question.find(params[:id])
+        @question.destroy
+        flash[:notice]="成功"
+        redirect_to ("/questions")
     end
 
 
@@ -28,4 +52,12 @@ class QuestionsController < ApplicationController
     def question_params
         params.require(:question).permit(:title, :body)
     end
+
+    def ensure_correct_user
+        @question = Question.find_by(id: params[:id])
+        if @question.user_id != current_user.id
+          flash[:alert] = "権限がありません"
+          redirect_to("/questions/#{@question.id}")
+        end
+      end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,8 +10,12 @@ Rails.application.routes.draw do
   get "/users/:id", to: "users#show"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'questions#index'
-  get "/questions", to: "questions#index"
-  get "/questions/new", to: "questions#new"
-  post "/questions", to: "questions#create"
-  get "/questions/:id", to: "questions#show"
+  resources :questions
+  # get "/questions", to: "questions#index"
+  # get "/questions/new", to: "questions#new"
+  # post "/questions", to: "questions#create"
+  # get "/questions/:id", to: "questions#show"
+  # get "/questions/:id/edit", to: "questions#edit"
+  # patch "/questions/:id", to: "questions#update"
+  # delete "/questions/:id", to: "questions#destroy"
 end
