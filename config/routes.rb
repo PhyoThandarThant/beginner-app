b26e81a85942c8b77182a03b2f2468014ed844e9
PhyoTDT <phyothandarthant@cybermissions.co.jp>
2021-11-19 16:17:49 +0900

    QuestionAnswerapplication complete

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,9 @@ Rails.application.routes.draw do
   get "/users/:id", to: "users#show"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'questions#index'
-  resources :questions
+  resources :questions do
+    resources :answers, only: [:create]
+  end
   # get "/questions", to: "questions#index"
   # get "/questions/new", to: "questions#new"
   # post "/questions", to: "questions#create"
